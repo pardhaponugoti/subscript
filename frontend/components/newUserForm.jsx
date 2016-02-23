43b381ce44bd8da84074b81b84472334cb70e264
pardhaponugoti <pardhapon@gmail.com>
2016-02-23 09:33:11 -0800

    finish sign up and log in pages html

--- frontend/components/newUserForm.jsx
+++ frontend/components/newUserForm.jsx
@@ -1,5 +1,16 @@
 var React = require('react');
 
+function isNumeric(n) { return !isNaN(parseFloat(n)) && isFinite(n); }
+function containsNumber(str) {
+  var containsNum = false;
+  for(var i = 0; i < str.length; i++) {
+    if (isNumeric(str[i])) {
+      containsNum = true;
+    }
+  }
+  return containsNum;
+}
+
 
 var NewUserForm = React.createClass({
   getInitialState: function() {
@@ -24,10 +35,26 @@ var NewUserForm = React.createClass({
       confirmPassword: e.target.value
     });
   },
-  passwordLi: function() {
+  passwordUl: function() {
     var passwordErrors = [];
-    if (this.state.password.length < 8 && this.state.password !== "") {
-      passwordErrors.push("Password must be at least 8 characters long");
+    if (this.state.password.length < 8 && this.state.password.length > 0) {
+      passwordErrors.push(<li><strong><font color="red">
+        Password must be at least 8 characters long
+      </font></strong></li>);
+    } else if (this.state.password.length > 0){
+      passwordErrors.push(<li><font color="green">
+        Password must be at least 8 characters long
+      </font></li>);
+    }
+
+    if (this.state.password.length > 0 && containsNumber(this.state.password)) {
+      passwordErrors.push(<li><font color="green">
+        Password must contain a number
+      </font></li>);
+    } else if (this.state.password.length > 0){
+      passwordErrors.push(<li><strong><font color="red">
+        Password must contain a number
+      </font></strong></li>);
     }
 
     if (passwordErrors.length === 0) {
@@ -35,17 +62,29 @@ var NewUserForm = React.createClass({
     } else {
       return <ul>
         {passwordErrors.map(function(error){
-          return <li>{error}</li>;
+          return error;
         })}
       </ul>;
     }
   },
+  matchedPassword: function() {
+    if (this.state.password !== "" && this.state.password.length > 7 && containsNumber(this.state.password)) {
+      if (this.state.password !== this.state.confirmPassword && this.state.confirmPassword !== "") {
+        return <div><strong><font color="red">
+          Passwords do not match!
+        </font></strong></div>;
+      } else if (this.state.confirmPassword !== "" && this.state.confirmPassword === this.state.password) {
+        return <div><font color="green">
+          Passwords match!
+        </font></div>;
+      }
+    } else {
+      return;
+    }
+
+  },
   render: function() {
     var csrfToken = $('meta[name=csrf-token]').attr('content');
-    var matchedPassword = "";
-    if (this.state.password !== this.state.confirmPassword && this.state.confirmPassword !== "") {
-      matchedPassword = "Passwords do not match!";
-    }
     return <form action="/users" method="post" className="new-user-form">
       <input name="authenticity_token" type="hidden" value={csrfToken} />
       <label>Email
@@ -57,13 +96,13 @@ var NewUserForm = React.createClass({
         <input type="password" name="user[password]" value={this.state.password}
             onChange={this.passwordChange} />
       </label>
-        {this.passwordLi()}
+        {this.passwordUl()}
       <br/>
       <label>Confirm Password
         <input type="password" value={this.state.confirmPassword}
             onChange={this.confirmPasswordChange} />
       </label>
-      <div>{matchedPassword}</div>
+      {this.matchedPassword()}
       <br/>
       <input type="submit"/>
     </form>;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
 
 var NewUserForm = require('./components/newUserForm');
 var NewSessionForm = require('./components/newSessionForm.jsx');
 
+// var routes = (
+//   <Routes>
+//     <Route path="/session/new" component={NewSessionForm}></Route>
+//     <Route path="/users/new" component={NewUserForm}></Route>
+//   </Routes>
+// );
+//
+// document.addEventListener("DOMContentLoaded", function () {
+//   ReactDOM.render(
+//     <Router>{routes}</Router>,
+//     document.getElementById('root')
+//   );
+// });
 
 // New User Form
 
